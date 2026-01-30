26230d301736237f46ac1ba6b4bf805f19376c9d
Wai Lee Boo <waileeboo@gmail.com>
2026-01-30 15:39:17 +0000

    combine price_return and price plot to 1 file

--- backend/src/eda/price_plots.py
+++ backend/src/eda/price_plots.py
@@ -2,13 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from typing import List
 from src.data_utils.data_loader import load_raw_data
+from src.data_utils.preprocess import add_return_features
+from src.utils.config import DEFAULT_TICKERS, FEATURE_COLS
 
 
-
-def plot_close_prices(
-    data: dict[str, pd.DataFrame],
-    tickers: List[str],
-) -> None:
+def plot_close_prices_stock(data: dict[str, pd.DataFrame], tickers: List[str]= DEFAULT_TICKERS) -> None:
     """
     Plot closing prices over time for the specified tickers.
     """
@@ -21,11 +19,34 @@ def plot_close_prices(
         plt.title(f"{ticker} Close Price Over Time", fontsize=16)
         plt.xlabel("Date")
         plt.ylabel("Price")
-        plt.grid(True, alpha=0.3)
+        plt.grid(True)
         plt.legend()
         plt.show()
 
 
+def plot_return_prices_stock(data: dict[str, pd.DataFrame], tickers: List[str] = DEFAULT_TICKERS, features: List[str] = FEATURE_COLS) -> None:
+    """
+    Plot return prices over time for the specified tickers.
+    """
+    
+    for ticker in tickers:
+        df = data[ticker]
+        df = add_return_features(df, features)
+        plt.figure(figsize=(14, 6))
+        plt.plot(df["Close_return"], label="Close Return Price")
+        plt.title(f"{ticker} Close Return Price Over Time")
+        plt.xlabel("Date")
+        plt.ylabel("Return Price")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+        
 if __name__ == "__main__":
     data = load_raw_data()
-    plot_close_prices(data, ["AAPL", "MSFT", "GOOGL", "AMZN"])
+    plot_return_prices_stock(data, tickers=DEFAULT_TICKERS, features=FEATURE_COLS)
+    # data2 = load_raw_data()
+    # plot_close_prices(data2, tickers=DEFAULT_TICKERS)
+    
+
+
+    
