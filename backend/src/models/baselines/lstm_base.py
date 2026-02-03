90ef93ac9d9c9e8f3deb18d54ec5f235a4fda6b1
Wai Lee Boo <waileeboo@gmail.com>
2026-02-03 22:45:40 +0000

    add dropout to lstm even if layer=1

--- backend/src/models/baselines/lstm_base.py
+++ backend/src/models/baselines/lstm_base.py
@@ -14,25 +14,37 @@ class LSTMBase(nn.Module):
             input_size=num_features,
             hidden_size=hidden_size,
             num_layers=num_layers,
-            dropout=dropout,
+            dropout=dropout if num_layers > 1 else 0,
+            # Data shape: (batch, seq_len, input_size) when using DataLoader so set Batch_first=True
             batch_first=True
         )
+        self.dropout = nn.Dropout(dropout)
         
         self.fc = nn.Linear(hidden_size, output_size)  
-        # self.fc = nn.Sequential(
+        
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out, _ = self.lstm(x)
+        last_out = out[:, -1, :]
+        last_out = self.dropout(last_out)
+        y_hat = self.fc(last_out)
+        return y_hat.squeeze(-1)  
+    
+    
+    
+    
+    # self.fc = nn.Sequential(
         #     nn.Linear(hidden_size, 32),
         #     nn.ReLU(),
         #     nn.Dropout(0.2),
         #     nn.Linear(32, output_size)
         # )
         
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out, _ = self.lstm(x)
-        last_out = out[:, -1, :]
-        y_hat = self.fc(last_out)
-        return y_hat.squeeze(-1)  
-    
-    
+        
+        
+        
+
+
 # class BiLSTMBase(nn.Module):
 #     def __init__(self, num_features: int = NUM_FEATURES, hidden_size=64, num_layers=1, dropout=0.3):
 #         super().__init__()
